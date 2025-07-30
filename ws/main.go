81621a33b0ea28e6c612e1ae34c63dda91f5efe3
Liam Aguilar <ayden@localhost.localdomain>
2025-07-30 17:08:04 -0600

    pull de ws again

--- ws/main.go
+++ ws/main.go
@@ -273,7 +273,7 @@ func (game *Game) gameLoop() {
 	fmt.Println("Ganó la partida", winner.Name)
 
 	game.Stats.Winner = winner.Name
-	game.Stats.TimeElapsed = int(time.Since(globalTime))
+	game.Stats.TimeElapsed = int(time.Since(globalTime).Seconds())
 	game.Stats.Players = game.Party.Members
 
 	fmt.Println(game.Stats)
