4de89a99659d15637d762c29338b68b1867a2487
Kevin Garcia <kvngarciameza@gmail.com>
2025-08-04 13:28:26 -0700

    lcl ldrbrd added

--- GOLF_OF/app/LeaderBoard.tsx
+++ GOLF_OF/app/LeaderBoard.tsx
@@ -1,25 +1,25 @@
-import React, { useEffect, useState } from 'react'; 
-import { View, StyleSheet } from 'react-native';
-import Leaderboard from '@/components/UserComponents/LeaderBoard';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import Leaderboard from "@/components/UserComponents/LeaderBoard";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
-import { LeaderboardStructure } from '@/components/UserComponents/LeaderBoard';
-const LeaderBoardScreen = () => { 
+import { LeaderboardStructure } from "@/components/UserComponents/LeaderBoard";
+const LeaderBoardScreen = () => {
   const [userData, setUserData] = useState([]);
   const [leaderboard, setLeaderboard] = useState<LeaderboardStructure[]>([]);
   useEffect(() => {
     const fetchUserCards = async () => {
       try {
-        const storedData = await AsyncStorage.getItem('latestUserCards');
+        const storedData = await AsyncStorage.getItem("latestUserCards");
         if (storedData) {
           const parsedData = JSON.parse(storedData);
-          console.log('Datos cargados desde AsyncStorage:', parsedData);
+          console.log("Datos cargados desde AsyncStorage:", parsedData);
           setUserData(parsedData);
         } else {
-          console.log('No hay datos guardados en AsyncStorage.');
+          console.log("No hay datos guardados en AsyncStorage.");
         }
       } catch (error) {
-        console.error('Error leyendo AsyncStorage:', error);
+        console.error("Error leyendo AsyncStorage:", error);
       }
     };
 
@@ -28,13 +28,13 @@ const LeaderBoardScreen = () => {
 
   console.log("Se imprime esto por el local storage", userData);
   useEffect(() => {
-    axios.get('http://127.0.0.1:8080/users/leaderboard')
-      .then(res => setLeaderboard(res.data))
-      .catch(err => console.error(err));
+    axios
+      .get("http://127.0.0.1:8080/users/leaderboard")
+      .then((res) => setLeaderboard(res.data))
+      .catch((err) => console.error(err));
   }, []);
 
   return <Leaderboard data={leaderboard} />;
 };
-
 
 export default LeaderBoardScreen;
---
+++ GOLF_OF/app/LocalLeaderBoard.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import Leaderboard from "@/components/UserComponents/localLdrBoard";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import { LocalLeaderboardStructure } from "@/components/UserComponents/localLdrBoard";
+const LeaderBoardScreen = () => {
+  const [userData, setUserData] = useState([]);
+  const [leaderboard, setLeaderboard] = useState<LocalLeaderboardStructure[]>(
+    []
+  );
+  // Muevale mi Javier, borra este mensaje al terminar su chamba atte: Kevin
+
+  // useEffect(() => {
+  //   const fetchUserCards = async () => {
+  //     try {
+  //       const storedData = await AsyncStorage.getItem("latestUserCards");
+  //       if (storedData) {
+  //         const parsedData = JSON.parse(storedData);
+  //         console.log("Datos cargados desde AsyncStorage:", parsedData);
+  //         setUserData(parsedData);
+  //       } else {
+  //         console.log("No hay datos guardados en AsyncStorage.");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error leyendo AsyncStorage:", error);
+  //     }
+  //   };
+
+  //   fetchUserCards();
+  // }, []);
+
+  // console.log("Se imprime esto por el local storage", userData);
+  // useEffect(() => {
+  //   // axios.get('http://127.0.0.1:8080/users/leaderboard')
+  //     .then(res => setLeaderboard(res.data))
+  //     .catch(err => console.error(err));
+  // }, []);
+
+  return <Leaderboard data={leaderboard} />;
+};
+
+export default LeaderBoardScreen;
